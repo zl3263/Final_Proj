eb03668c2e30c26eda2b9b41e4366e152e1bf896
Aubry King <biorabbit@126.com>
2022-12-04 15:02:09 -0400

    fixing a typo and add boundarys on map

--- Shiny_Map/app.R
+++ Shiny_Map/app.R
@@ -7,12 +7,13 @@ library(sf)
 library(shinyWidgets)
 library(geojsonio)
 
-block_edge = geojsonio::geojson_read("query.json",what  = "sp")
+block_edge = geojsonio::geojson_read("zoning_boundary.json",what  = "sp")
+load("cleaned_data.RData")
 #block_edge =  geojsonio::geojson_read("E:/Data_Science/Final_Proj/data/2000 Census Blocks.geojson",what  = "sp") 
 #block_edge <- readLines("E:/Data_Science/Final_Proj/data/2000 Census Blocks.geojson") %>% paste(collapse = "\n") 
 #load("cache.RData")
 
-# Define UI for application that draws a histogram
+# Define UI for application
 ui <- bootstrapPage(
   tags$head(
     tags$link(href = "https://fonts.googleapis.com/css?family=Oswald", rel = "stylesheet"),
@@ -40,8 +41,13 @@ ui <- bootstrapPage(
                 ')
   ),
   
-  leafletOutput("map", width = "100%", height = "100%"),
-  
+  leafletOutput("map", width = "100%", height = "80%"),
+  dataTableOutput("record"),
+  absolutePanel(
+    top = 10, right = 10, style = "z-index:500; text-align: right;",
+    tags$h2("Browse our dataset by zoning"),
+    tags$a("About this tool", href="https://cultureofinsight.com/portfolio/crimewatch/")
+  ),
 
 )
 
@@ -52,9 +58,18 @@ server <- function(input, output) {
         leaflet(block_edge)%>%
         addProviderTiles("CartoDB.Positron")%>%
         setView(lng = -73.90, lat = 40.7, zoom = 12) %>%
-        addPolygons()
+        addPolygons(
+          smoothFactor = 0.3,
+          color = "#444444",
+          weight=1,
+          fillColor = "transparent",
+          label = ~ZONEDIST
+        )
         #addGeoJSON(block_edge, weight = 1, color = "#444444", fill = FALSE)
     })
+    
+    output$record
+    
 }
 
 # Run the application 
