c7ef3e995581e9a3850079be79f1e7c37a754f76
Aubry King <biorabbit@126.com>
2022-12-03 23:05:35 -0400

    somehow, we can show a map now

--- Shiny_Map/app.R
+++ Shiny_Map/app.R
@@ -1,42 +1,56 @@
-
 library(shiny)
 library(rsconnect)
+library(tidyverse)
+library(leaflet)
+library(htmlwidgets)
+library(sf)
+library(shinyWidgets)
+library(geojsonio)
+
+#block_edge = rgdal::readOGR("data/2000 Census Blocks.geojson") 
+
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
+ui <- bootstrapPage(
+  tags$head(
+    tags$link(href = "https://fonts.googleapis.com/css?family=Oswald", rel = "stylesheet"),
+    tags$style(type = "text/css", "html, body {width:100%;height:100%; font-family: Oswald, sans-serif;}"),
+    tags$script(src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.16/iframeResizer.contentWindow.min.js",
+                type="text/javascript"),
+    tags$script('
+                $(document).ready(function () {
+                  navigator.geolocation.getCurrentPosition(onSuccess, onError);
+                
+                  function onError (err) {
+                    Shiny.onInputChange("geolocation", false);
+                  }
+                
+                  function onSuccess (position) {
+                    setTimeout(function () {
+                      var coords = position.coords;
+                      console.log(coords.latitude + ", " + coords.longitude);
+                      Shiny.onInputChange("geolocation", true);
+                      Shiny.onInputChange("lat", coords.latitude);
+                      Shiny.onInputChange("long", coords.longitude);
+                    }, 1100)
+                  }
+                });
+                ')
+  ),
+  
+  leafletOutput("map", width = "100%", height = "100%"),
+  
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+    output$map <- renderLeaflet({
+        leaflet()%>%
+        addProviderTiles("CartoDB.Positron")%>%
+        setView(-3, 54.3, zoom = 6)
+        #addTopoJSON(block_edge, weight = 1, color = "#444444", fill = FALSE)
     })
 }
 
