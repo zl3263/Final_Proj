28423df55ab8d09f24a3dfe9cae36f09776a1dd9
Aubry King <biorabbit@126.com>
2022-12-05 18:41:33 -0400

    some fancy interaction

--- Shiny_Map/app.R
+++ Shiny_Map/app.R
@@ -17,15 +17,11 @@ load("cleaned_data.RData")
 #block_edge <- readLines("zoning_boundary.json") %>% paste(collapse = "\n") 
 #load("cache.RData")
 block_edge=read_sf("DTM_Tax_Block_Polygon.shp") %>%
-  st_transform("NAD83") %>%
-  mutate(borough=as.numeric(BORO)) %>%
-  select(borough,everything())%>%
-  nest(data=OBJECTID:geometry) %>%
-  mutate(data = map(data,sf_geojson)) %>%
-  na.omit() %>%
-  arrange(borough)
+  st_transform("+proj=longlat +datum=WGS84") %>%
+  mutate(BORO=as.numeric(BORO),BLOCK=as.numeric(BLOCK)) %>%
+  arrange(BORO)
 
-
+this_year= as.numeric(substr(Sys.Date(),1,4))
 
 # Define UI for application
 ui <- bootstrapPage(
@@ -56,10 +52,10 @@ ui <- bootstrapPage(
   ),
   
   leafletOutput("map", width = "100%", height = "70%"),
-  textOutput("test"),
+  span(textOutput("test"), style="color:red"),
   dataTableOutput("record"),
   absolutePanel(
-    top = 10, left = 10, style = "z-index:500; text-align: left;",
+    top = 10, left = 10, style = "z-index:500; text-align: left; background-color: rgba(255,255,255,0.4);",
     tags$h2("Browse our dataset by Borough-Block"),
     tags$a("Back to Main Page", href="https://zl3263.github.io/Final_Proj/"),
     selectInput(
@@ -74,6 +70,30 @@ ui <- bootstrapPage(
       ),
       selected = 1,
       width = "30%"
+    ),
+    radioButtons(
+      "display_option",
+      label = "Select display option",
+      choices = list(
+        "Display all blocks" = 1,
+        "Display blocs with data" = 2
+      ),
+      selected = 1,
+      inline = TRUE
+    ),
+    sliderInput(
+      "built_year",
+      label = "Select building built year",
+      min = 1840,
+      max = this_year,
+      value = c(1840,this_year)
+    ),
+    sliderInput(
+      "report_year",
+      label = "Select data report year",
+      min = 2012,
+      max = this_year,
+      value = c(2012,this_year)
     )
   ),
 
@@ -82,10 +102,14 @@ ui <- bootstrapPage(
 
 # Define server logic
 server <- function(input, output) {
-
+    block_edge_new = 
+      block_edge %>%
+      filter(BORO==1) %>%
+      sf_geojson()
+      
     output$map <- renderLeaflet({
-      leaflet(block_edge,options = leafletOptions(zoomControl = FALSE))%>%
-      addProviderTiles("CartoDB.Positron")%>%
+      leaflet(options = leafletOptions(zoomControl = FALSE)) %>%
+      addProviderTiles("CartoDB.Positron") %>%
       setView(lng = -73.974, lat = 40.762, zoom = 12) %>%
       #addPolygons(
       #  smoothFactor = 0.3,
@@ -94,7 +118,7 @@ server <- function(input, output) {
       #  fillColor = "transparent",
       #  label = ~paste(as.character(BORO),"-",as.character(BLOCK))
       #)
-      addGeoJSON(block_edge$data[[1]], weight = 1, color = "#444444", fillColor = "transparent")
+      addGeoJSON(block_edge_new, weight = 1, color = "#444444", fillColor = "transparent")
     })
     output$test <- renderText({
       "Please select block"
@@ -117,22 +141,58 @@ server <- function(input, output) {
     })
     observe({
       selected_boro = as.numeric(input$selected_boro)
-      output$map <- renderLeaflet({
-        leaflet(block_edge,options = leafletOptions(zoomControl = FALSE))%>%
-          addProviderTiles("CartoDB.Positron")%>%
-          setView(lng = -73.974, lat = 40.762, zoom = 12) %>%
-          #addPolygons(
-          #  smoothFactor = 0.3,
-          #  color = "#444444",
-          #  weight=1,
-          #  fillColor = "transparent",
-          #  label = ~paste(as.character(BORO),"-",as.character(BLOCK))
-          #)
-          addGeoJSON(block_edge$data[[selected_boro]], weight = 1, color = "#444444", fillColor = "transparent")
-      })
+      display_option = as.numeric(input$display_option)
+      #block_edge_selected
+      
+      if(display_option == 1){
+        block_edge_selected =
+          block_edge %>%
+          filter(
+            BORO == selected_boro
+          ) %>%
+          sf_geojson()
+      }
+      else{
+        selected_block = 
+          transformed_rental_income %>%
+          filter(
+            year_built >= input$built_year[1],
+            year_built <= input$built_year[2],
+            report_year >= input$report_year[1],
+            report_year <= input$report_year[2]
+          ) %>%
+          distinct(block_id) %>%
+          pull(block_id)
+        
+        
+        block_edge_selected =
+          block_edge %>%
+          filter(
+            BORO == selected_boro,
+            BLOCK %in% selected_block
+          ) %>%
+          sf_geojson()
+      }
+      
+      leafletProxy("map") %>%
+        clearGeoJSON() %>%
+        addGeoJSON(block_edge_selected, weight = 1, color = "#444444", fillColor = "transparent")
+        
+      #output$map <- renderLeaflet({
+      #  leaflet(options = leafletOptions(zoomControl = FALSE)) %>%
+      #    addProviderTiles("CartoDB.Positron") %>%
+      #    setView(lng = -73.974, lat = 40.762, zoom = 12) %>%
+      #    #addPolygons(
+      #    #  smoothFactor = 0.3,
+      #    #  color = "#444444",
+      #    #  weight=1,
+      #    #  fillColor = "transparent",
+      #    #  label = ~paste(as.character(BORO),"-",as.character(BLOCK))
+      #    #)
+      #    addGeoJSON(block_edge_selected, weight = 1, color = "#444444", fillColor = "transparent")
+      #})
     })
     observe({
-      leafletProxy("map") %>% clearPopups()
       event <- input$map_geojson_click
       if (is.null(event))
         return()
@@ -145,13 +205,29 @@ server <- function(input, output) {
             "-",
             as.character(input$map_geojson_click$properties$BLOCK)
           )
-          
         })
       })
+      
+      selected_block =
+        block_edge %>%
+        filter(
+          BORO == as.numeric(input$map_geojson_click$properties$BORO),
+          BLOCK ==  as.numeric(input$map_geojson_click$properties$BLOCK)
+        )
+      
+      leafletProxy("map") %>%
+        clearShapes() %>%
+        addPolygons(stroke = FALSE, fillColor  = "red", fillOpacity=0.1,data= selected_block)
       
       output$record <- renderDataTable({
         transformed_rental_income %>%
           filter(boro_block == as.numeric(input$map_geojson_click$properties$BORO)*100000+as.numeric(input$map_geojson_click$properties$BLOCK)) %>%
+          filter(
+            year_built >= input$built_year[1],
+            year_built <= input$built_year[2],
+            report_year >= input$report_year[1],
+            report_year <= input$report_year[2]
+          ) %>%
           select(
             boro_block,
             address,
